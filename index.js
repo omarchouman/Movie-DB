016e49839c5c4d2360692dd383db28093aba3804
Omar Chouman <omar.chouman0@gmail.com>
2021-06-14 10:28:34 +0300

    step 4

--- index.js
+++ index.js
@@ -26,4 +26,39 @@ app.get("/time", (req, res) => {
     res.send(final);
 })
 
+// Hello ID Route
+app.get("/hello/:id", (req, res) => {
+    const message = `Hello, ${req.params.id}`
+    const final = {
+        status: 200,
+        message: message
+    }
+    res.send(final)
+})
+
+// Search Route
+app.get("/search", (req, res) => {
+    let status, message, data, final, error
+    if(req.query.s){
+        status = 200;
+        message = "ok";
+        data = req.query.s;
+        final = {
+            status: status,
+            message: message,
+            data: data
+        }
+    } else {
+        status = 500;
+        message = "you have to provide a search";
+        error = true;
+        final = {
+            status: status,
+            message: message,
+            error: error
+        }
+    }
+    res.send(final);
+})
+
 app.listen(3000);
